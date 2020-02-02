1dd08d819cae20103bbfa67734450caea2daf7c8
freak_lather <ajaysinghlather@gmail.com>
2020-02-02 12:31:00 +0800

    Initial Commit

---
+++ iContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iContacts
+//
+//  Created by ajay lather on 02/02/20.
+//  Copyright © 2020 Ajay Lather. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iContactsTests/iContactsTests.swift
@@ -0,0 +1,34 @@
+//
+//  iContactsTests.swift
+//  iContactsTests
+//
+//  Created by ajay lather on 02/02/20.
+//  Copyright © 2020 Ajay Lather. All rights reserved.
+//
+
+import XCTest
+@testable import iContacts
+
+class iContactsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
